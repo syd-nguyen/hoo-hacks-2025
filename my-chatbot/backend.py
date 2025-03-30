f173b0dad97beec2a6008d4a17a6e890d52e65e7
geibb <ttrkendallkow@gmail.com>
2025-03-30 02:29:45 -0400

    backend update!

--- my-chatbot/backend.py
+++ my-chatbot/backend.py
@@ -1,8 +1,23 @@
-from flask import Flask
+from flask import Flask, request, jsonify
+from flask_cors import CORS
+from google import genai
 
+#initialize flask
 app = Flask(__name__)
+CORS(app)
 
-@app.route("/")
-def home():
-    return "Hello World, from Flask!"
+#initialize gemini
+client = genai.Client(api_key="AIzaSyBmfWgIR1BCF66sLIU3RqVODQFM3ybyqsk")
 
+@app.route("/chat", methods=['POST'])
+def chat():
+    data = request.json
+    user_message = data.get("message", "")
+    
+    #generate ai response
+    response = client.models.generate_content(
+        model="gemini-2.0-flash",
+        contents=["give me 5 elementary german sentences"]
+    )
+
+    return response.text
