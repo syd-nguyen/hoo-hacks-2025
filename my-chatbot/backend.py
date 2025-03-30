a8d2a2b66937d48937c41e7a9ade4cf322e3c3ef
geibb <ttrkendallkow@gmail.com>
2025-03-30 03:22:49 -0400

    backend update again

--- my-chatbot/backend.py
+++ my-chatbot/backend.py
@@ -1,4 +1,4 @@
-from flask import Flask, request, jsonify
+from flask import Flask, request, jsonify, render_template
 from flask_cors import CORS
 from google import genai
 
@@ -9,15 +9,22 @@ CORS(app)
 #initialize gemini
 client = genai.Client(api_key="AIzaSyBmfWgIR1BCF66sLIU3RqVODQFM3ybyqsk")
 
+
+@app.route("/")
+def home():
+    return render_template("index.html")
+
 @app.route("/chat", methods=['POST'])
 def chat():
     data = request.json
     user_message = data.get("message", "")
+    return jsonify({"response": generate_ai_response(user_message)})
     
+
+def generate_ai_response(user_message):
     #generate ai response
     response = client.models.generate_content(
         model="gemini-2.0-flash",
-        contents=["give me 5 elementary german sentences"]
+        contents=["Generate a short response (2-3 sentences) to this message: " + user_message + ". Use the japanese language with an elementary school level proficiency. "]
     )
-
     return response.text
